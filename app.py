0c66ffc3fb0b1ccfd73d3aad82726ae36632cf64
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2025-01-25 17:22:02 -0300

    CRUD part 1

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask
+from flask import Flask, request, jsonlify
+from models.task import Task
 
 app = Flask(__name__)
 
-@app.route("/")
-def helloWorld():
-    return "Hello World!"
+tasks = []
+taskIdControl = 1
 
-@app.route("/about")
-def about():
-    return "Página Sobre"
+@app.route("/tasks", methods=["POST"])
+def createTask():
+    global taskIdControl
+    data = request.get_json()
+    newTask = Task(id=taskIdControl ,title=data["title"], description=data.get("description", ""))
+    taskIdControl += 1
+    tasks.append(newTask)
+    return jsonlify({"message": "Nova tarefa criada com sucesso!"})
+
 
 if(__name__ == "__main__"):
     app.run(debug=True)
