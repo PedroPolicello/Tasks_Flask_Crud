99c8c37da0154cfc33c96d5b30781c0a9b0554ab
Pedro Policello <88664809+PedroPolicello@users.noreply.github.com>
2025-01-26 14:51:46 -0300

    Add READ & UPDATE

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonlify
+from flask import Flask, request, jsonify
 from models.task import Task
 
 app = Flask(__name__)
@@ -6,6 +6,7 @@ app = Flask(__name__)
 tasks = []
 taskIdControl = 1
 
+#CREATE
 @app.route("/tasks", methods=["POST"])
 def createTask():
     global taskIdControl
@@ -13,7 +14,44 @@ def createTask():
     newTask = Task(id=taskIdControl ,title=data["title"], description=data.get("description", ""))
     taskIdControl += 1
     tasks.append(newTask)
-    return jsonlify({"message": "Nova tarefa criada com sucesso!"})
+    return jsonify({"message": "Nova tarefa criada com sucesso!"})
+
+#READ
+@app.route("/tasks", methods=["GET"])
+def getTasks():
+    taskList = [task.toDict() for task in tasks]
+
+    output = {
+        "tasks": taskList,
+        "totalTasks": len(taskList)
+    }
+    return jsonify(output)
+
+#READ
+@app.route("/tasks/<int:id>", methods=["GET"])
+def getTask(id):
+    for t in tasks:
+        if t.id == id:
+            return jsonify(t.toDict())
+        
+    return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+
+#UPDATE
+@app.route("/tasks/<int:id>", methods=["PUT"])
+def updateTask(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+
+    if task == None:
+        return jsonify({"message": "Não foi possivel encontrar a atividade"}), 404
+    
+    data = request.get_json()
+    task.title = data ["title"]
+    task.description = data ["description"]
+    task.completed = data ["completed"]
+    return jsonify({"message": "Tarefa atualizada com sucesso!"})
 
 
 if(__name__ == "__main__"):
